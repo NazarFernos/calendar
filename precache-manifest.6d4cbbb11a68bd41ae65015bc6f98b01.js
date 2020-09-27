0100cfff76167adfe688c7493fe0dcfe8a29dbb8
nazar.fernos <nazarum98@gmail.com>
2020-09-27 13:00:59 +0300

    Updates

---
+++ precache-manifest.6d4cbbb11a68bd41ae65015bc6f98b01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fee26d1de3277cbe5a647ff575fd57",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "0406d23fe357e768de88",
+    "url": "/calendar/static/css/main.01b7c106.chunk.css"
+  },
+  {
+    "revision": "b0b8eb68f3dce0e24389",
+    "url": "/calendar/static/js/2.463ba0d1.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/calendar/static/js/2.463ba0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0406d23fe357e768de88",
+    "url": "/calendar/static/js/main.ea7cf6dd.chunk.js"
+  },
+  {
+    "revision": "d61ece5a1070ff7d7347",
+    "url": "/calendar/static/js/runtime-main.c4248c1f.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/calendar/static/media/background.37a75b54.jpg"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/calendar/static/media/logo.b1a38376.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9949c4d2143d95ab72bb5b3f487ae0a.js"
+  "/calendar/precache-manifest.6d4cbbb11a68bd41ae65015bc6f98b01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calendar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
