f6c75ac1b06a177038ade38afa0aed56cad0a8c4
nazar.fernos <nazarum98@gmail.com>
2020-09-27 03:00:15 +0300

    Updates

---
+++ precache-manifest.a3c8fa46fd6458cbd6b1cb1d289c15cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2cf428afb22def55f89f9edcdde5c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c78967208498c68c67f",
+    "url": "/static/css/main.10bcbb44.chunk.css"
+  },
+  {
+    "revision": "14bf81540c75f1e5eda2",
+    "url": "/static/js/2.d49369ad.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.d49369ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c78967208498c68c67f",
+    "url": "/static/js/main.e6e769a1.chunk.js"
+  },
+  {
+    "revision": "08a9beebeb02caae0676",
+    "url": "/static/js/runtime-main.4ebfe112.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/static/media/background.37a75b54.jpg"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/static/media/logo.b1a38376.svg"
+  }
+]);
